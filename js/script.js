3a761939f724fa5562c6c77d7412ceb2fd9a2e23
LENGUYENHIEU <nguyenhieu10102004@gmail.com>
2024-04-19 09:58:15 +0700

    BaiTapLonWeb

---
+++ js/script.js
@@ -0,0 +1,9 @@
+<script>
+    // JavaScript for mobile menu toggle
+    const toggleButton = document.querySelector('.navbar-toggler');
+    const mobileMenu = document.querySelector('.navbar-collapse');
+
+    toggleButton.addEventListener('click', function() {
+        mobileMenu.classList.toggle('show');
+    {"}"});
+</script>
